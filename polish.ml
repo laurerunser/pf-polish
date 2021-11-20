638c811e5ad66e1f7d36e7b17746c85f13333575
Laure Runser <laure-runser@tutanota.com>
2021-11-20 16:53:06 +0100

    Add methods to read lines and parse expr and conds
    Add mehods :
    - to read all the lines from a file and associated them with their
    line number
    - compute the indent of a line
    - parse an expression
    - parse a condition

--- polish.ml
+++ polish.ml
@@ -48,9 +48,69 @@ type program = block
 
 
 (***********************************************************************)
+open List
+(* Opens the file `filename` and reads all the lines.
+Returns a list that associates each line with its line number. *)  
+let read_all_lines (filename:string) : (int * string) list =
+  let in_chan = open_in filename in
+  let try_read = try Some (input_line in_chan) with End_of_file -> None in
+  let rec read_lines acc p = match try_read with
+    | Some s -> read_lines ((p, s)::acc) (p+1)
+    | None -> close_in in_chan; List.rev acc in
+  read_lines [] 1 (* the first line is number 1 *)
 
-let read_polish (filename:string) : program = failwith "TODO"
+(* computes the indentation of a line of words.
+The identation is the number of spaces / 2 at the beginning of the line *)
+let compute_indent (words:string list) : int =
+  let rec loop w acc = match w with
+    | [] -> acc
+    | ""::xs -> loop xs (acc+1)
+    | x::xs -> acc
+  in (loop words 0) / 2
 
+
+(* parse the list of words to get the arithmetic expression
+Returns the expr and a list of the rest of the words  *)
+let rec parse_expr words =
+  let s = hd words in
+  let ops = ["+"; "-"; "*"; "/"; "%"] in
+  if (mem s ops) then
+    let e, rest = parse_expr (tl words) in
+    let e2, _ = parse_expr rest in
+    match s with
+    | "+" -> Op(Add, e, e2), []
+    | "-" -> Op(Sub, e, e2), []
+    | "*" -> Op(Mul, e, e2), []
+    | "/" -> Op(Div, e, e2), []
+    | "%" -> Op(Mod, e, e2), []
+    | _ -> raise (Failure "Invalid operator in expr")
+  else
+    try Num(int_of_string s), tl words
+    with Failure _ -> Var(s), tl words            
+
+let parse_cond words =
+  let e1, rest = parse_expr words in
+  let cmp_word = hd rest in
+  let e2, rest = parse_expr (tl rest) in
+  let cmp = 
+    match cmp_word with
+    | "=" -> Eq
+    | "<>" -> Ne
+    | "<" -> Lt
+    | "<=" -> Le
+    | ">" -> Gt
+    | ">=" -> Ge
+    | _ -> raise (Failure "Invalid comparaison operator in condition") in
+  e1, cmp, e2
+                        
+
+
+(* *)
+let read_polish (filename:string) : program = 
+  (* read all the lines and associate them with their line numbers *)
+  let lines_list = read_all_lines filename in
+
+                                            
 let print_polish (p:program) : unit = failwith "TODO"
 
 let eval_polish (p:program) : unit = failwith "TODO"
