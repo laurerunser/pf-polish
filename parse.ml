5595c0ef0a3520c0776af98213e85aac4bc78305
Laure Runser <laure-runser@tutanota.com>
2021-11-25 14:28:20 +0100

    Add the interface file for Parse and write doc

--- parse.ml
+++ parse.ml
@@ -1,18 +1,8 @@
 open Polish
-
 open List
 open Printf
-   
-(* Opens the file `filename` and reads all the lines.
-Returns a list that associates each line with its line number. *)  
+    
 let read_all_lines (filename:string) : (string) list =
-(*  let in_chan = open_in filename in
-  let try_read = try Some (input_line in_chan) with End_of_file -> None in
-  let rec read_lines acc p = match try_read with
-    | Some s -> read_lines ((p, s)::acc) (p+1)
-    | None -> close_in in_chan; List.rev acc in
-    printf "read all the lines\n";
-  read_lines [] 1 (* the first line is number 1 *) *)
   let ic = open_in filename in
   let try_read () =
     try Some (input_line ic) with End_of_file -> None in
@@ -21,23 +11,15 @@ let read_all_lines (filename:string) : (string) list =
     | None -> close_in ic; List.rev acc in
   loop []
 
-
-(* changes the list from (int*string) to (int*(string list)) by seperating
-the line into words. Doesn't remove the empty strings (in between the spaces 
-if there are several next to each other) *)
 let lines_to_words lines =
   let to_words l =
     let position, str = l in
     position, String.split_on_char ' ' str in
   map (fun l -> to_words l) lines
 
-(* remove all the lines that have the COMMENT key-word inside *)
 let remove_comments lines =
   filter (fun l -> let p,w = l in not (mem "COMMENT" w)) lines
 
-
-(* computes the indentation of a line of words.
-The identation is the number of spaces / 2 at the beginning of the line *)
 let compute_indent (words:string list) : int =
   let rec loop w acc = match w with
     | [] -> acc
@@ -45,15 +27,6 @@ let compute_indent (words:string list) : int =
     | x::xs -> acc
   in (loop words 0) / 2
 
-
-let rec print_arr w =
-  match w with
-  | [] -> print_string "ok\n"
-  | x::xs -> printf "%s " x; print_arr xs       
- 
-
-(* parse the list of words to get the arithmetic expression
-Returns the expr and a list of the rest of the words  *)
 let rec parse_expr words =
   if words = [] then failwith "Invalid line"
   else
@@ -74,12 +47,6 @@ let rec parse_expr words =
               with Failure _ -> Var(s) in
       i, (tl words)
 
-
-
-(* Finds the comparaison operator in the list of words.
-Returns the operator and 2 lists :
-- the first one contains the elements before the operator in the original list
-- the second one contains the elements after the operator in the original list*)
 let find_cmp_and_partition words =
   let rec loop acc l =
     match l with
@@ -95,14 +62,12 @@ let find_cmp_and_partition words =
        | _ -> loop (x::acc) xs
   in loop [] words
 
-(* Parses the list of words into a condition*)
 let parse_cond words =
   let cmp, l1, l2 = find_cmp_and_partition words in
   let e1, _ = parse_expr l1 in
   let e2, _ = parse_expr l2 in
   e1, cmp, e2
 
-(* returns true if the next line begins by ELSE, false otherwise *)
 let next_line_is_else lines =
   if lines = [] then false
   else
@@ -152,12 +117,11 @@ and parse_block lines indent acc =
 
       
 let parse_file (filename:string) : program =
-  (* read all the lines and associate them with their line numbers *)
-  let lines = read_all_lines filename in
-  let lines_with_positions =
+  let lines = read_all_lines filename in (* read the lines *)
+  let lines_with_positions = (* associate them with their line numbers *)
     mapi (fun k str -> (k, str)) lines in
-  let lines_words = lines_to_words lines_with_positions in
-  let lines_clean = remove_comments lines_words in
-  let prgm, _ = parse_block lines_clean 0 [] in
+  let lines_words = lines_to_words lines_with_positions in (* splits them into words *)
+  let lines_clean = remove_comments lines_words in (* remove the comments *)
+  let prgm, _ = parse_block lines_clean 0 [] in (* parse into a program *)
   prgm
 
---
+++ parse.mli
@@ -0,0 +1,72 @@
+(* Module Parse
+This module handles the parsing of a .p file into a program.
+ *)
+
+open Polish (* needed for types definitions *)
+
+val parse_file : string -> program
+(* [parse_file filename] parses the file into a polish program. *)
+
+val parse_instruction : int -> string list -> (int * (string list))
+                        -> instr * (int * (string list)) list
+(* [parse_instruction position s lines] parses the next instruction that starts
+with the line s at the position position. 
+If needed, it will parse the rest of the lines to complete the instruction 
+(in the case of an IF or WHILE instruction).
+
+Returns the instruction and the list of lines that were not parsed. *)
+
+val parse_block : (int * (string list)) -> int
+                  -> (int * instr) list * (int * (string list)) list
+(* [parse_block lines ident acc] parses the block starting from the first
+element of lines. 
+It parses all the instructions of the block, until the indent of the line goes
+back up (= it is the end of the block).
+All the instructions are stored into the acc argument, along with their line position.
+
+Returns the list of instructions and the list of lines that were not parsed. *)
+  
+val parse_expr : (string list) -> expr
+(* [parse_expr words] parses the list of words into an expression.
+Fails if the expression cannot be parsed. *)                                    
+
+val parse_cond : (string list) -> cond
+(* [parse_cond words] parses the list of word into a condition *)  
+  
+val read_all_lines :  string -> string list
+(* [read_all_lines filename] reads all the lines from that file 
+and returns them in a list.
+This methods does NOT handle errors in opening the file, it assumes
+the file exists and can be opened. *)                                  
+
+val lines_to_words : (int * string) list -> (int * (string list)) list
+(* [lines_to_words lines] splits each line into words.
+- lines is a list that associate each line number with its line
+- the empty words (if there are 2 spaces next to each other) are not removed *)
+
+val remove_comments : (int * string) list -> (int * string) list
+(* [remove_comments lines] removes all the lines that contain the
+word "COMMENT" from the list (even if it is not the first word of the line). 
+
+Possible amelioration : only remove the line if "COMMENT" is the first word.
+But that would add a lot of work for nothing. I think it is better to treat
+"COMMENT" as a restricted keyword of the language. *)
+
+val compute_indent : (string list) -> int
+(* [compute_indent words] computes the identation of a list of words.
+The identation is half of the number of empty words at the beginning of the list
+(nb empty words / 2). *)
+
+val find_cmp_and_partition : (string list) -> comp * (string list) * (string list)
+(* [find_cmp_and_partition words] finds the comparaison operation in the list
+of words. Then it partitions the list to get 2 sublists :
+- the first one with all the elements that come before the operator in the list
+- the second one with all the elements that come after the operator in the list
+The operator is in neither of the two new sublists.
+
+Fails if it cannot find an operator in the words list. *)
+                                                                       
+val next_line_is_else : (int * (string list)) list -> boolean
+(* [next_line_is_else lines] returns true if the first line of lines
+is an "ELSE" instruction. *)
+                                                        
