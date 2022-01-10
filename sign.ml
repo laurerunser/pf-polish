0c99a1c6ec0a71bd7b9fdea10dbbaa43ad149b33
Laure Runser <laure-runser@tutanota.com>
2022-01-10 18:07:46 +0100

    Sign : add the line number for the divisionby zero errors. Also change the default signs for variables that are not initialized : I made them [Neg;Zero;Pos] instead of [Error] so that only the divisions by zero will be triggering the errors

--- sign.ml
+++ sign.ml
@@ -25,7 +25,8 @@ let remove_duplicates l =
   if List.mem Error l then Error::add_pos else add_pos
   
 
-let sign_add s1 s2 =
+let sign_add s1 s2 current_line error_line =
+  let signs = 
     match s1, s2 with
     | Zero, Zero -> [Zero]
     | Zero, Pos | Pos, Zero -> [Pos]
@@ -34,26 +35,30 @@ let sign_add s1 s2 =
     | Neg, Neg -> [Neg]
     | Pos, Neg | Neg, Pos -> [Neg; Zero; Pos]
     | Error, _ | _, Error -> [Error]
+  in signs, error_line
 
-let sign_sub s1 s2 =
+let sign_sub s1 s2 current_line error_line =
   let s1 = reverse_sign s1 in
   let s2 = reverse_sign s2 in
-  sign_add s1 s2
+  sign_add s1 s2 current_line error_line
 
-let sign_mul s1 s2 =
-  match s1, s2 with
-  | Error, _| _, Error -> [Error]
-  | _, Zero -> [Zero]
-  | Zero, _ -> [Zero]
-  | Pos, Neg | Neg, Pos -> [Neg]
-  | Pos, Pos | Neg, Neg -> [Pos]
+let sign_mul s1 s2 current_line error_line =
+  let signs = 
+    match s1, s2 with
+    | Error, _| _, Error -> [Error]
+    | _, Zero -> [Zero]
+    | Zero, _ -> [Zero]
+    | Pos, Neg | Neg, Pos -> [Neg]
+    | Pos, Pos | Neg, Neg -> [Pos]
+  in signs, error_line
 
-let sign_div s1 s2 =
+let sign_div s1 s2 current_line error_line =
   match s1, s2 with
-  | Error, _| _, Error | _, Zero -> [Error]
-  | Zero, _ -> [Zero]
-  | Pos, Neg | Neg, Pos -> [Neg]
-  | Pos, Pos | Neg, Neg -> [Pos]
+  | Error, _| _, Error -> [Error], error_line
+  | _, Zero -> [Error], if error_line = -1 then current_line else error_line (* send back the number of the line *)
+  | Zero, _ -> [Zero], error_line
+  | Pos, Neg | Neg, Pos -> [Neg], error_line
+  | Pos, Pos | Neg, Neg -> [Pos], error_line
 (* We could have combined both sign_mul and sign_div into a single function, with a
 boolean to know which case we were on. 
 But then we would not have been able to use those functions as first-class arguments 
@@ -61,10 +66,10 @@ in the find_sign function because all the functions to find  the sign would not 
 had the same type.
 I though this solution was the best one, and the one that required the least amount of code *)
   
-let sign_mod s1 s2 =
+let sign_mod s1 s2 current_line error_line =
   match s1, s2 with
-  | Error, _ | _, Error | _, Zero ->  [Error]
-  | s1, s2 -> [s1]
+  | Error, _ | _, Error | _, Zero ->  [Error], error_line
+  | s1, s2 -> [s1], error_line
   (* The Pervasives documentation states that : "Note that x mod y is negative only if x < 0."
     So the mod is always the same sign than the dividend (unless y=0, then error) *)
 
@@ -75,35 +80,40 @@ let signs_num n =
 
 let signs_var n signs =
   let signs_list = Signs.find_opt n signs in
-  if Option.is_none signs_list then [Error] (* This var was not initialized before use *)
+  if Option.is_none signs_list then [Neg; Zero; Pos] (* This var was not initialized before use *)
   else Option.get signs_list (* The var was already initialized so we return its sign list*)
 
-let rec loop_expr e signs_list signs =
+let rec loop_expr e signs_list signs current_line error_line =
   match e with
-  | Num(n) -> signs_num n
-  | Var(n) -> signs_var n signs
+  | Num(n) -> signs_num n, error_line
+  | Var(n) -> signs_var n signs, error_line
   | Op(op, e1, e2) ->
-    let l1 = loop_expr e1 signs_list signs in
-    let l2 = loop_expr e2 signs_list signs in
-    match op with
-    | Add -> find_sign l1 l2 sign_add signs_list signs []
-    | Sub -> find_sign l1 l2 sign_sub signs_list signs []
-    | Mul -> find_sign l1 l2 sign_mul signs_list signs []
-    | Div -> find_sign l1 l2 sign_div signs_list signs []
-    | Mod -> find_sign l1 l2 sign_mod signs_list signs []
-and find_sign l1 l2 sign_fun signs_list signs acc =
-  let rec loop s1 l2 acc =
+    let l1, error_line1 = loop_expr e1 signs_list signs current_line error_line in
+    let l2, error_line2 = loop_expr e2 signs_list signs current_line error_line in
+    let error_line = if error_line1 = -1 then error_line2 else min error_line1 error_line2 in
+    let f = (match op with
+    | Add -> sign_add
+    | Sub -> sign_sub
+    | Mul -> sign_mul
+    | Div -> sign_div
+    | Mod -> sign_mod)
+    in find_sign l1 l2 f signs_list signs [] current_line error_line
+and find_sign l1 l2 sign_fun signs_list signs acc current_line error_line =
+  let rec loop s1 l2 acc error_line =
     match l2 with
-    | [] -> acc
+    | [] -> acc, error_line
     | s2::xs -> 
-      loop s1 xs (acc @ (sign_fun s1 s2))
+      let acc2, error_line = sign_fun s1 s2 current_line error_line in
+      loop s1 xs (acc @ acc2) error_line
  in match l1 with
- | [] -> remove_duplicates acc
- | y::ys -> find_sign ys l2 sign_fun signs_list signs (loop y l2 acc)
+ | [] -> remove_duplicates acc, error_line
+ | y::ys -> 
+    let acc2, error_line = loop y l2 acc error_line in
+    find_sign ys l2 sign_fun signs_list signs acc2 current_line error_line
 
-let find_signs_expr n e (signs:sign list Signs.t) =
+let find_signs_expr n e (signs:sign list Signs.t) current_line error_line =
   let list_e = Signs.find_opt n signs in
-  loop_expr e list_e signs
+  loop_expr e list_e signs current_line error_line
 
 (********************************************)
 let add_signs var s_to_add signs_list signs =
@@ -175,9 +185,9 @@ let rec compare_2_signs s1 s2 comp =
   | Pos, Pos | Neg, Neg -> true
   | Pos, Neg -> compare_2_signs s2 s1 (reverse_comp comp)
 
-let compare_2_expr e1 e2 comp signs =
+let compare_2_expr e1 e2 comp signs current_line error_line =
   let signs_e1 = signs_var e1 signs in
-  let signs_e2 = find_signs_expr e1 (Var(e1)) signs in
+  let signs_e2, error_line = find_signs_expr e1 (Var(e1)) signs current_line error_line in
   let rec loop_e2 s1 l2 b acc =
     match l2 with
     | [] -> b, acc
@@ -194,59 +204,69 @@ let compare_2_expr e1 e2 comp signs =
       if b2 then loop_e1 xs b2 (remove_duplicates (x::acc1)) (remove_duplicates acc_e@acc2)
       else loop_e1 xs b acc1 acc2
     in
-  loop_e1 signs_e1 false [] []
+  loop_e1 signs_e1 false [] [], error_line
 
 
-let rec sign_cond c signs = 
+let rec sign_cond c signs current_line (error_line:int) = 
   let e1, comp, e2 = c in
   match e1, e2 with
-  | Num(a), Num(b) -> if a > b then true, signs else false, signs
-  | Var(var), Num(n) -> compare_var_and_num var n comp signs
-  | Num(n), Var(var) -> compare_var_and_num var n (reverse_comp comp) signs
+  | Num(a), Num(b) -> if a > b then true, signs, error_line else false, signs, error_line
+  | Var(var), Num(n) -> 
+      let b, signs = compare_var_and_num var n comp signs in b, signs, error_line
+  | Num(n), Var(var) -> 
+    let b, signs = compare_var_and_num var n (reverse_comp comp) signs in b, signs, error_line
   | Var(var), e2 -> 
-    let b, signs_var, signs_e = compare_2_expr var e2 comp signs in
+    let (b, signs_var, signs_e), error_line = compare_2_expr var e2 comp signs current_line error_line in
     let signs = Signs.add var signs_var (Signs.remove var signs) in
     (* updates the signs of the var *)
     (* TODO ?? maybe should also determine stuff about the vars inside the expr (if any)
        with the new info we got about the sign of the expression *)
-    b, signs
-  | e1, Var(var) -> sign_cond (Var(var), reverse_comp comp, e1) signs
+    b, signs, error_line
+  | e1, Var(var) -> sign_cond (Var(var), reverse_comp comp, e1) signs current_line error_line
   | _ -> failwith ""
-
+(* TODO : missing Num,Op and Op,Num*)
 
 (**********************************************)
 
-let rec signs_program p signs =
+let rec signs_program p signs error_line =
   match p with
-  | [] -> signs
-  | (i, Print(e))::xs  -> signs_program xs signs
-  | (i, Read(n))::xs -> signs_program xs(Signs.add n [Neg; Zero; Pos] signs)
-  | (i, Set(n, e))::xs -> signs_program xs (Signs.add n (find_signs_expr n e signs) signs)
-  | (i, If(c, b, b2))::xs -> signs_program xs (signs_if c b b2 signs)
-  | (i, While(c, b))::xs -> signs_program xs (signs_while c b signs)
-and signs_if c b b2 signs = 
+  | [] -> signs, error_line
+  | (i, Print(e))::xs  -> signs_program xs signs error_line
+  | (i, Read(n))::xs -> signs_program xs(Signs.add n [Neg; Zero; Pos] signs) error_line
+  | (i, Set(n, e))::xs -> 
+      let signs_list, error_line = find_signs_expr n e signs i error_line in
+      signs_program xs (Signs.add n signs_list signs) error_line
+  | (i, If(c, b, b2))::xs -> 
+      let signs, error_line = signs_if c b b2 signs i error_line in
+      signs_program xs signs error_line
+  | (i, While(c, b))::xs -> 
+      let signs, error_line = signs_while c b signs i error_line in
+      signs_program xs signs error_line
+and signs_if c b b2 signs current_line error_line = 
   (* See if the condition can be satisfied and compute the signs propagation *)
-  let res1, signs1 = sign_cond c signs in
+  let res1, signs1, error_line = sign_cond c signs current_line error_line in
   let e1, comp, e2 = c in
-  let res2, signs2 = sign_cond (e1, reverse_comp comp, e2) signs in
+  let res2, signs2, error_line = sign_cond (e1, reverse_comp comp, e2) signs current_line error_line in
   (* If the condition can be satisfied, compute the signs of the block *)
-  let signs1 = if res1 then signs_program b signs1 else Signs.empty in 
-  let signs2 = if res2 then signs_program b2 signs2 else Signs.empty in
+  let signs1, error_line1 = if res1 then signs_program b signs1 error_line else Signs.empty, error_line in 
+  let signs2, error_line2 = if res2 then signs_program b2 signs2 error_line else Signs.empty, error_line in
+  let error_line = if error_line1 = -1 then error_line2 else min error_line1 error_line2 in
   (* Join both environments *)
   Signs.merge (fun var x y -> 
                 if Option.is_none x then y
                 else if Option.is_none y then x
                 else Some(remove_duplicates (Option.get x)@(Option.get y))) 
               signs1 signs2
-and signs_while c b signs = signs
-
+  , error_line
+and signs_while c b signs current_line error_line = signs, error_line
 
 let print_signs p =
-  let signs = signs_program p Signs.empty in
+  let signs, error_line = signs_program p Signs.empty (-1) in
+  let error_str = if error_line = -1 then "safe\n" else "divbyzero" ^ string_of_int error_line ^ "\n" in
   let rec print_list s_list =
     match s_list with
     | [] -> print_newline()
-    | x::xs -> 
+    | x::xs -> (* TODO : make sure the signs in the sign list come in the right order *)
       match x with
       | Neg -> print_string "-"; print_list xs
       | Pos -> print_string "+"; print_list xs
@@ -255,5 +275,5 @@ let print_signs p =
   in
   Signs.iter (fun x signs_list -> 
               print_string x; print_string " : "; 
-              print_list signs_list) signs
-(* TODO : make sure the signs in the sign list come in the right order *)
+              print_list (remove_duplicates signs_list)) signs;
+  print_string error_str
