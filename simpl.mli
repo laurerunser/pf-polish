10fdaec99b9d9d921274c97dba3d6463d6d803d1
Laure Runser <laure-runser@tutanota.com>
2022-01-10 20:47:40 +0100

    Simpl : add doc to the .mli

--- simpl.mli
+++ simpl.mli
@@ -1,4 +1,9 @@
 open Polish
 
 val simplify_block : program -> int NameTable.t -> program
+(*[simplify_block block env] returns the simplified block.
+  env contains the current values of the variables that have
+  been set to an int. *)
+
 val simplify_expr : expr -> int NameTable.t -> expr
+(*[simplify_expr e env] returns the simplified expression. *)
